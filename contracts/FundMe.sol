8713315ab09572ea27190b85aac13451ea648338
Ramchandhar Rapolu <hiramrapolu@gmail.com>
2024-06-10 23:20:58 +0100

    adding constants and immutable to save on gas

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -11,22 +11,41 @@ contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUSD = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
+    // 818,208 gas - non-constant
+    // 797,620 gas - constant
+    // 797,620 gas - constant
+
+    address public immutable i_owner;
 
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
     address[] public funders;
 
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUSD, "didn't send enough ETH/USD, it should be atleast $5"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
-        funders.push(msg.sender);
+    constructor()  {
+        i_owner = msg.sender;
+        // immutable
+        // 2577 gas - non-immutable
+        // 444 gas - immutable
     }
-
     
-    function withdraw() public {
+    //only owner of this contract can withdraw
+    modifier onlyOwner {
+        require(msg.sender == i_owner, "Must be Owner!");
+        _;
+    }
+    
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH/USD, it should be atleast $5"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+    
+    function withdraw() public onlyOwner {
         // for loop
         // [1,2,3,4] elements
         // 0,1,2,3 indexes
         // for (/* starting index, ending index, step amount*/ )
+        
         for(uint256 funderIndex; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -45,14 +64,13 @@ contract FundMe {
         //payable(msg.sender) = payable address
 
         //transfer method, gas is set to 2300, if it fails, it throws an error and reverts the txn.
-        payable(msg.sender).transfer(address(this).balance);
+        // payable(msg.sender).transfer(address(this).balance);
         //send method, gas is set to 2300, if it fails, it returns true or false
-        bool sendSuccess = payable (msg.sender).send(address(this).balance);
+        // bool sendSuccess = payable (msg.sender).send(address(this).balance);
         // send will only revert the transaction  if the below statement is added.
-        require(sendSuccess, "Send Failed");
+        // require(sendSuccess, "Send Failed");
         //call
         (bool callSuccess, ) = payable (msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call Failed");
-        
     }
 }
