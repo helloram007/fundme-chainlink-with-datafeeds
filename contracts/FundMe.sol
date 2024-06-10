5897b64419f6fe6eecc2b04063775808739faf1f
Ramchandhar Rapolu <hiramrapolu@gmail.com>
2024-06-10 22:42:48 +0100

    adding behaviour for withdraw using transfer,send,call methods

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,5 +1,10 @@
+// Get Funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
+
 import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
@@ -11,23 +16,43 @@ contract FundMe {
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
     address[] public funders;
 
-    // for any function to send or receive funds is make the function payable
     function fund() public payable {
-        //Allow users to send $
-        // Have a minimum $ sent
-        // 1. How do we send ETH to this contract?
-        // for ETH to receive or send, you need to use the variable msg.value
-        // if you need to check any condition you require function
-
         require(msg.value.getConversionRate() >= minimumUSD, "didn't send enough ETH/USD, it should be atleast $5"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
-
-        // When anyone funds, we will store them in an array called funders
-
         funders.push(msg.sender);
-
-        // What is a revert?
-        // Undo any actions that have been done, and send the remaining gas back
-        // Here in the above e.g When the require is success, then myValue is saved as dmyValue + 1, else the action is reverted.
     }
-    //function withdraw() public {}
+
+    
+    function withdraw() public {
+        // for loop
+        // [1,2,3,4] elements
+        // 0,1,2,3 indexes
+        // for (/* starting index, ending index, step amount*/ )
+        for(uint256 funderIndex; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // reset the array
+        funders = new address[](0);
+        // actually withdraw the funds
+        
+        // three different ways to send
+        //transfer
+        //send
+        //call
+
+        //msg.sender = address
+        //payable(msg.sender) = payable address
+
+        //transfer method, gas is set to 2300, if it fails, it throws an error and reverts the txn.
+        payable(msg.sender).transfer(address(this).balance);
+        //send method, gas is set to 2300, if it fails, it returns true or false
+        bool sendSuccess = payable (msg.sender).send(address(this).balance);
+        // send will only revert the transaction  if the below statement is added.
+        require(sendSuccess, "Send Failed");
+        //call
+        (bool callSuccess, ) = payable (msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+        
+    }
 }
