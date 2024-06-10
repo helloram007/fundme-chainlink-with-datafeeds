872a3304c59be2486fde779122447a3a0dbdc6c7
Ramchandhar Rapolu <hiramrapolu@gmail.com>
2024-06-10 21:55:03 +0100

    added behaviour, where a user funds, the address is stored with an amount in a map

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,6 +10,9 @@ contract FundMe {
 
     uint256 public minimumUSD = 5e18;
 
+    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
+    address[] public funders;
+
     // for any function to send or receive funds is make the function payable
     function fund() public payable {
         //Allow users to send $
@@ -19,7 +22,8 @@ contract FundMe {
         // for ETH to receive or send, you need to use the variable msg.value
         // if you need to check any condition you require function
         require(getConversionRate(msg.value) >= minimumUSD, "didn't send enough ETH/USD, it should be atleast $5"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
-
+        // When anyone funds, we will store them in an array called funders
+        funders.push(msg.sender);
         // What is a revert?
         // Undo any actions that have been done, and send the remaining gas back
         // Here in the above e.g When the require is success, then myValue is saved as dmyValue + 1, else the action is reverted.
